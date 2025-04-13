caa212d8edc6c34405c4da1bbf92437d0372cee9
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2025-04-13 19:03:23 +0200

    mobile html markup
    todo: modals and popups

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,8 @@
 import { Config } from "tailwindcss";
-const Config = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-    "./index.html",
-    "./src/**/*.{html,js}",
-  ],
+export default {
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        tertiary: "var(--color-tertiary)",
-        textPrimary: "var(--color-text-primary)",
-        textSecondary: "var(--color-text-secondary)",
-        background: "var(--color-background)",
-        strawberry: "var(--color-strawberry)",
-        banana: "var(--color-banana)",
-        watermelon: "var(--color-watermelon)",
-        lime: "var(--color-lime)",
-        apple: "var(--color-apple)",
-        peach: "var(--color-peach)",
-        plums: "var(--color-plums)",
-        orange: "var(--color-orange)",
-        kiwi: "var(--color-kiwi)",
-        raspberry: "var(--color-raspberry)",
-      },
-    },
+    extend: {},
   },
 
   plugins: [],
